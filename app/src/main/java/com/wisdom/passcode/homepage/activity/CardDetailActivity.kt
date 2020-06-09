cedfb057a64eb55785921b84a31508c8dfcc6160
Escort <1007214131@qq.com>
2020-06-09 17:19:58 +0800

    亮码记录完成

--- app/src/main/java/com/wisdom/passcode/homepage/activity/CardDetailActivity.kt
+++ app/src/main/java/com/wisdom/passcode/homepage/activity/CardDetailActivity.kt
@@ -6,22 +6,25 @@ import com.wisdom.passcode.ConstantString
 import com.wisdom.passcode.ConstantString.Companion.MY_CODE_PASS_TYPE_NORMAL
 import com.wisdom.passcode.ConstantString.Companion.MY_CODE_PASS_TYPE_WORKDAYS
 import com.wisdom.passcode.R
+import com.wisdom.passcode.apply.activity.CarCardApplyActivity
+import com.wisdom.passcode.apply.activity.PersonCardApplyActivity
 import com.wisdom.passcode.base.BaseActivity
 import com.wisdom.passcode.base.SharedPreferenceUtil
 import com.wisdom.passcode.homepage.model.CodeListModel
 import com.wisdom.passcode.util.EncrypAndDecrypUtil
 import com.wisdom.passcode.util.QrCodeUtils
 import kotlinx.android.synthetic.main.activity_card_detail.*
+import org.jetbrains.anko.startActivity
 import java.text.SimpleDateFormat
 
-class CardDetailActivity : BaseActivity() {
+class CardDetailActivity : BaseActivity(), View.OnClickListener {
 
-
+    var tag=""
     @SuppressLint("SimpleDateFormat")
     override fun initViews() {
         iv_close.setOnClickListener { finish() }
         val ifDateOf = intent.getStringExtra("outOffDate")
-        val tag = intent.getStringExtra("tag")
+         tag = intent.getStringExtra("tag")
         val data = intent.extras.getSerializable("data") as CodeListModel
         //生成二维码
         //对placeId进行加密操作
@@ -39,7 +42,9 @@ class CardDetailActivity : BaseActivity() {
         }
         //根据不同卡证设置不同的背景
         setUiEffect(ifDateOf, tag, data)
-
+//重新申请点击事件
+        tv_reapplay.setOnClickListener(this)
+        btn_reapply.setOnClickListener(this)
     }
 
     override fun setlayoutIds() {
@@ -83,9 +88,9 @@ class CardDetailActivity : BaseActivity() {
                         //即将过期
                         rl_code.setBackgroundResource(R.drawable.kz_a_big)
                         rl_out_of_date.visibility = View.GONE
-                        btn_reapply.visibility=View.VISIBLE
-                        tv_date_useful.visibility=View.VISIBLE
-                        tv_out_off_date.visibility=View.VISIBLE
+                        btn_reapply.visibility = View.VISIBLE
+                        tv_date_useful.visibility = View.VISIBLE
+                        tv_out_off_date.visibility = View.VISIBLE
 
                     }
                     else -> {
@@ -100,7 +105,8 @@ class CardDetailActivity : BaseActivity() {
             ConstantString.DETAIL_PERSON_CARD -> {
                 //人员通行证
                 with(data) {
-                    tv_line_1.text = "【${SharedPreferenceUtil.getPersonalInfoModel(this@CardDetailActivity).nickName}】人员出入证"
+                    tv_line_1.text =
+                        "【${SharedPreferenceUtil.getPersonalInfoModel(this@CardDetailActivity).nickName}】人员出入证"
                     val limit = when (codeTypeDataType) {
                         MY_CODE_PASS_TYPE_WORKDAYS -> {
                             "工作日"
@@ -127,12 +133,12 @@ class CardDetailActivity : BaseActivity() {
                         //即将过期
                         rl_code.setBackgroundResource(R.drawable.kz_b_big)
                         rl_out_of_date.visibility = View.GONE
-                        btn_reapply.visibility=View.VISIBLE
-                        tv_date_useful.visibility=View.VISIBLE
-                        tv_out_off_date.visibility=View.VISIBLE
+                        btn_reapply.visibility = View.VISIBLE
+                        tv_date_useful.visibility = View.VISIBLE
+                        tv_out_off_date.visibility = View.VISIBLE
                     }
                     else -> {
-                        //                    没过期
+                        //没过期
                         rl_code.setBackgroundResource(R.drawable.kz_b_big)
                         rl_out_of_date.visibility = View.GONE
                     }
@@ -155,6 +161,30 @@ class CardDetailActivity : BaseActivity() {
 
             }
         }
+    }
+
+    /**
+     *  @describe 页面内点击事件
+     *  @return
+     *  @author HanXueFeng
+     *  @time 2020/6/9 0009  17:08
+     */
+    override fun onClick(v: View?) {
+       when(v!!.id){
+           R.id.tv_reapplay,R.id.btn_reapply->{
+//               重新申请按钮点击事件
+               when(tag){
+                   ConstantString.DETAIL_CAR_CARD->{
+                       //车辆通行证重新申请
+                       startActivity<CarCardApplyActivity>()
+                   }
+                   ConstantString.DETAIL_PERSON_CARD->{
+                       //人员通行证重新申请
+                       startActivity<PersonCardApplyActivity>()
+                   }
+               }
+           }
+       }
     }
 
 
